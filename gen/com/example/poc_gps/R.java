f8dd521a5793b4fe94dabdacdd7b12a75d0d2d6f
gabriel-farache <gabriel.farache@gmail.com>
2013-09-20 17:31:07 +0200

    Others auto generated modifications

--- gen/com/example/poc_gps/R.java
+++ gen/com/example/poc_gps/R.java
@@ -182,7 +182,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020018;
     }
     public static final class id {
-        public static final int action_settings=0x7f05000d;
+        public static final int action_settings=0x7f05000e;
         public static final int cache=0x7f050006;
         public static final int cleanGPSCache=0x7f050008;
         public static final int cleanWifiCache=0x7f050007;
@@ -190,10 +190,11 @@ containing a value of this type.
         public static final int displayMap=0x7f05000a;
         public static final int displayWifiCache=0x7f050009;
         public static final int hybrid=0x7f050004;
-        public static final int map=0x7f05000c;
+        public static final int map=0x7f05000d;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int rideToDisplay=0x7f05000b;
+        public static final int sampleTime=0x7f05000c;
         public static final int satellite=0x7f050002;
         public static final int terrain=0x7f050003;
     }
