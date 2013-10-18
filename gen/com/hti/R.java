67c74e462fc3decbddc44645827c65c45b3d0734
ThomasCarayol <carayol@etud.insa-toulouse.fr>
2013-10-18 22:34:51 +0200

    R.java pour pouvoir pull

--- gen/com/hti/R.java
+++ gen/com/hti/R.java
@@ -21,31 +21,67 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int ic_run=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080003;
-        public static final int pager=0x7f080000;
-        public static final int pager_title_strip=0x7f080001;
-        public static final int section_label=0x7f080002;
+        public static final int action_forgot_password=0x7f08000e;
+        public static final int action_settings=0x7f08000f;
+        public static final int button1=0x7f08000d;
+        public static final int email=0x7f080003;
+        public static final int fragment_main_result_ride=0x7f08000a;
+        public static final int fragment_main_run=0x7f080009;
+        public static final int login=0x7f080005;
+        public static final int login_form=0x7f080002;
+        public static final int login_status=0x7f080000;
+        public static final int login_status_message=0x7f080001;
+        public static final int pager=0x7f080007;
+        public static final int pager_title_strip=0x7f080008;
+        public static final int password=0x7f080004;
+        public static final int sign_in_button=0x7f080006;
+        public static final int textView1=0x7f08000c;
+        public static final int userEmailAndWeight=0x7f08000b;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_main_dummy=0x7f030001;
+        public static final int activity_login=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_main_result_ride=0x7f030002;
+        public static final int fragment_main_run=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int login=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
+        public static final int action_forgot_password=0x7f050014;
         public static final int action_settings=0x7f050001;
+        public static final int action_sign_in_register=0x7f050012;
+        public static final int action_sign_in_short=0x7f050013;
         public static final int app_name=0x7f050000;
+        public static final int buttonLogin=0x7f05000d;
+        public static final int error_field_required=0x7f050019;
+        public static final int error_incorrect_password=0x7f050018;
+        public static final int error_invalid_email=0x7f050016;
+        public static final int error_invalid_password=0x7f050017;
+        public static final int hintLogin=0x7f050009;
+        public static final int hintPassword=0x7f05000b;
+        public static final int login_progress_signing_in=0x7f050015;
         public static final int messageCache=0x7f050007;
         public static final int messageDisplay=0x7f050008;
         public static final int messageGPS=0x7f050005;
         public static final int messageWifi=0x7f050006;
+        /**  Strings related to login 
+         */
+        public static final int prompt_email=0x7f050010;
+        public static final int prompt_password=0x7f050011;
+        public static final int showLogin=0x7f05000a;
+        public static final int showPassword=0x7f05000c;
+        public static final int title_activity_login=0x7f05000f;
         public static final int title_section1=0x7f050002;
         public static final int title_section2=0x7f050003;
         public static final int title_section3=0x7f050004;
+        public static final int welcomeText=0x7f05000e;
     }
     public static final class style {
         /** 
@@ -74,5 +110,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int LoginFormContainer=0x7f060002;
     }
 }
